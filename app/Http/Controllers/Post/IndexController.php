da6ebfaef5d0b93bb2fea0e94d23bbd32f65d755
vadim.garaev <68156261+3lyka@users.noreply.github.com>
2022-06-08 17:23:28 +0300

    master

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -5,14 +5,21 @@ namespace App\Http\Controllers\Post;
 /* use App\Http\Requests\StoreRequest; */
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\FilterRequest;
 use App\Models\Post;
 
 class IndexController extends Controller
 {
-	public function __invoke()
+	public function __invoke(FilterRequest $filterRequest)
 	{
 		// $post = Post::find(1); вызов конкретного айди
-		$posts = Post::paginate(12);
-		return view('post.index', compact('posts'));
+/* 		$posts = Post::paginate(12);
+		return view('post.index', compact('posts')); */
+		$data = $filterRequest->validated();
+		dd($data);
+		$post = Post::where('is_published', 1)
+		->where('category_id', 5)
+		->get();
+
 	}
 }
