b2b43504f3e43ae55ee22993952b97d86c8abdc0
vadim.garaev <68156261+3lyka@users.noreply.github.com>
2022-07-19 19:12:02 +0300

    a

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,20 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Admin panel</title>
-
-	<!-- Google Font: Source Sans Pro -->
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-	<!-- Font Awesome Icons -->
-	<link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
-	<!-- overlayScrollbars -->
-	<link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-	<!-- Theme style -->
-	<link rel="stylesheet" href="dist/css/adminlte.min.css">
-</head>
+@extends('layouts.admin')
+@section('content')
 
 <body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
 	<div class="wrapper">
@@ -296,35 +281,58 @@
 									</ul>
 								</li>
 
-						<li class="nav-item">
-							<a href="#" class="nav-link">
-								<i class="nav-icon fas fa-search"></i>
-								<p>
-									Search
-									<i class="fas fa-angle-left right"></i>
-								</p>
-							</a>
-							<ul class="nav nav-treeview">
 								<li class="nav-item">
-									<a href="pages/search/simple.html" class="nav-link">
-										<i class="far fa-circle nav-icon"></i>
-										<p>Simple Search</p>
+									<a href="#" class="nav-link">
+										<i class="nav-icon fas fa-search"></i>
+										<p>
+											Search
+											<i class="fas fa-angle-left right"></i>
+										</p>
 									</a>
-								</li>
-								<li class="nav-item">
-									<a href="pages/search/enhanced.html" class="nav-link">
-										<i class="far fa-circle nav-icon"></i>
-										<p>Enhanced</p>
-									</a>
+									<ul class="nav nav-treeview">
+										<li class="nav-item">
+											<a href="pages/search/simple.html" class="nav-link">
+												<i class="far fa-circle nav-icon"></i>
+												<p>Simple Search</p>
+											</a>
+										</li>
+										<li class="nav-item">
+											<a href="pages/search/enhanced.html" class="nav-link">
+												<i class="far fa-circle nav-icon"></i>
+												<p>Enhanced</p>
+											</a>
+										</li>
+									</ul>
 								</li>
 							</ul>
-						</li>
-					</ul>
 				</nav>
 				<!-- /.sidebar-menu -->
 			</div>
 			<!-- /.sidebar -->
 		</aside>
+
+		<div class="posts" style="margin-top:5% ;">
+			<div class="container">
+				<div class="row">
+					<form class="lh-lg" action="{{ route('post.store') }}" method="post">
+						@csrf
+						<div class="mb-3">
+							<label class="title">Title</label>
+							<input type="text" name="title" class="form-control" id="title" placeholder="Title">
+						</div>
+						<div class="mb-3">
+							<label class="title">content</label>
+							<textarea type="text" name="content" class="form-control" id="content" placeholder="content"></textarea>
+						</div>
+						<div class="mb-3">
+							<label class="title">Image</label>
+							<input type="text" name="image" class="form-control" id="image" placeholder="Image">
+						</div>
+						<button type="submit" class="btn btn-primary">Create</button>
+					</form>
+				</div>
+			</div>
+		</div>
 
 		<!-- Content Wrapper. Contains page content -->
 		<div class="content-wrapper">
@@ -348,31 +356,6 @@
 
 
 	</div>
-	<!-- ./wrapper -->
 
-	<!-- REQUIRED SCRIPTS -->
-	<!-- jQuery -->
-	<script src="plugins/jquery/jquery.min.js"></script>
-	<!-- Bootstrap -->
-	<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-	<!-- overlayScrollbars -->
-	<script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
-	<!-- AdminLTE App -->
-	<script src="dist/js/adminlte.js"></script>
 
-	<!-- PAGE PLUGINS -->
-	<!-- jQuery Mapael -->
-	<script src="plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
-	<script src="plugins/raphael/raphael.min.js"></script>
-	<script src="plugins/jquery-mapael/jquery.mapael.min.js"></script>
-	<script src="plugins/jquery-mapael/maps/usa_states.min.js"></script>
-	<!-- ChartJS -->
-	<script src="plugins/chart.js/Chart.min.js"></script>
-
-	<!-- AdminLTE for demo purposes -->
-	<script src="dist/js/demo.js"></script>
-	<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-	<script src="dist/js/pages/dashboard2.js"></script>
-</body>
-
-</html>
+	@endsection
